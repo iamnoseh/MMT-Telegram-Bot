ac4810ddf54d4a2f53a5d8ad760c15c4fea4c568
iamnoseh <noseh.tagaymurodzoda@gmail.com>
2025-02-24 15:06:16 +0500

    n

--- ImportQuestions/Program.cs
+++ ImportQuestions/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
@@ -5,13 +6,17 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TelegramBot.Domain.Entities;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Танзим кардани DataContext ва дигар сервисҳо
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Configure Kestrel to listen on a specific IP address and port
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(IPAddress.Any, 5000); // Барои гӯш кардани ҳама IP-ҳо дар порти 5000
+});
 
 builder.Services.AddControllersWithViews();
 
@@ -20,18 +25,18 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    app.UseHsts(); // Барои ҳифзи SSL
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.UseAuthorization();
 
+// Танзими роутинг
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=ImportMvc}/{action=Index}/{id?}");
 
+// Барои дастрасӣ танҳо бо HTTP (агар SSL сертификат истифода нашавад)
 app.Run();
