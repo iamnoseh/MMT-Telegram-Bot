22e1589c9d6f714020bdd8b0edaaaf8e30856480
Noseh Taghaymurodzoda <nosehtagaymurodzoda@gmail.com>
2025-06-18 13:17:40 +0500

    fix

--- TelegramBot/Services/TelegramBotHostedService.cs
+++ TelegramBot/Services/TelegramBotHostedService.cs
@@ -53,7 +53,7 @@ public class TelegramBotHostedService : IHostedService
     {
         _scopeFactory = scopeFactory;
         _configuration = configuration;
-        var token = "7418214830:AAGf1xCyFFIa-h2yVjFn7SaD6c069vsRJ8A"; // Replace with your actual token
+        var token = "8005745055:AAEIqUs8wgo9ANJkhgN7b7MWomjq4z5bRLw"; // Replace with your actual token
         _client = new TelegramBotClient(token);
         _channelId = configuration["TelegramChannel:ChannelId"] ?? throw new ArgumentNullException("ID-и канал ёфт нашуд!");
         _channelLink = configuration["TelegramChannel:ChannelLink"] ?? throw new ArgumentNullException("Пайванди канал ёфт нашуд!");
